cd7cd038d8b93d44fbae79446d3be4fce66dd442
Leo Borai <estebanborai@gmail.com>
2025-08-29 00:50:50 +0200

    revert: naive pagination (needs serious improvements)
    This reverts commit a6e1ca38d090a49bcba665fd2e244695e712e5f4.

--- src/server/src/modules/mtg/repository.rs
+++ src/server/src/modules/mtg/repository.rs
@@ -6,8 +6,6 @@ use deckmaster_domain::mtg::model::{Card, Deck};
 use deckmaster_domain::mtg::service::MtgDataAccessLayer;
 use deckmaster_domain::mtg::service::{FindCardsFilter, FindDecksFilter};
 
-const PAGE_SIZE: usize = 20;
-
 #[derive(Clone)]
 pub struct MtgRepository {
     cards: Arc<Vec<Card>>,
@@ -25,7 +23,7 @@ impl MtgRepository {
 
 impl MtgDataAccessLayer for MtgRepository {
     async fn find_cards(&self, filter: FindCardsFilter) -> Result<Vec<Card>> {
-        let mut qs = self
+        let qs = self
             .cards
             .to_vec()
             .iter()
@@ -39,30 +37,10 @@ impl MtgDataAccessLayer for MtgRepository {
             .cloned()
             .collect::<Vec<Card>>();
 
-        if let Some(page) = filter.page {
-            let start = (page as usize - 1) * PAGE_SIZE;
-            let end = (start + PAGE_SIZE).min(qs.len());
-            qs = qs[start..end].to_vec();
-        } else {
-            let end = PAGE_SIZE.min(qs.len());
-            qs = qs[..end].to_vec();
-        }
-
         Ok(qs)
     }
 
-    async fn find_decks(&self, filter: FindDecksFilter) -> Result<Vec<Deck>> {
-        let mut qs = self.decks.to_vec();
-
-        if let Some(page) = filter.page {
-            let start = (page as usize - 1) * PAGE_SIZE;
-            let end = (start + PAGE_SIZE).min(qs.len());
-            qs = qs[start..end].to_vec();
-        } else {
-            let end = PAGE_SIZE.min(qs.len());
-            qs = qs[..end].to_vec();
-        }
-
-        Ok(qs)
+    async fn find_decks(&self, _: FindDecksFilter) -> Result<Vec<Deck>> {
+        Ok(self.decks.to_vec())
     }
 }
