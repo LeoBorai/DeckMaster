1b5cfdb9afc0226f7b44093c72bf1555762eb64a
Leo Borai <estebanborai@gmail.com>
2025-09-28 15:13:23 +0200

    feat: minor tweaks

--- src/ui/src/components/template/Layout.tsx
+++ src/ui/src/components/template/Layout.tsx
@@ -4,14 +4,9 @@ import type { JSX } from "react";
 
 export function Layout(): JSX.Element {
   return (
-    <div className="bg-zinc-950 text-zinc-100 min-h-screen">
-      <header className="bg-zinc-900 flex items-center justify-between py-4 px-6">
-        <div className="flex items-center gap-3">
-          <figure>
-            <img src="https://via.placeholder.com/50" />
-          </figure>
-          <h1>DeckMaster</h1>
-        </div>
+    <div className="bg-zinc-900 text-zinc-100 min-h-screen">
+      <header className="bg-zinc-800 text-sm flex items-center justify-between py-2 px-4 h-9 border-b border-zinc-600">
+        <h1>DeckMaster</h1>
         <nav>
           <NavLink to="/">Search</NavLink>
         </nav>
