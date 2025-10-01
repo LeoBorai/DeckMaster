1111a7d8b0ca3223300646d2d60909dc6e7e668b
Leo Borai <estebanborai@gmail.com>
2025-10-02 01:59:41 +0200

    feat: logs for http tracing

--- src/server/src/router/mod.rs
+++ src/server/src/router/mod.rs
@@ -6,6 +6,8 @@ use std::sync::Arc;
 use anyhow::Result;
 use axum::routing::get;
 use axum::{Extension, Router};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -20,6 +22,11 @@ pub async fn make_router() -> Result<Router> {
         .nest("/api", api::make_api_router())
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiV0Doc::openapi()))
         .fallback_service(get(serve_asset))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO)),
+        )
         .layer(Extension(services));
 
     Ok(router)
